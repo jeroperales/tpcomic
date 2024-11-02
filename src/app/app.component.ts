25443e74787553e3e22cb59a2fb63f332af0adeb
jeroperales <jeronimoperales1@gmail.com>
2024-11-02 18:40:45 -0300

    hola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CharacterDetailComponent } from './components/character-detail/character-detail.component';
+import { CharacterCardComponent } from './components/character-card/character-card.component';
+import { CharacterFormComponent } from './components/character-form/character-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CharacterDetailComponent, CharacterCardComponent, CharacterFormComponent, CharacterFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
