5d799a0c3f23cbf98b5a2ef26bc2c3859987f3a8
jeroperales <jeronimoperales1@gmail.com>
2024-11-02 18:38:12 -0300

    agregue imports

---
+++ src/app/components/character-card/character-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './character-card.component.html',
+  styleUrl: './character-card.component.css'
+})
+export class CharacterCardComponent {
+  
+
+}
